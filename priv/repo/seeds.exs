f67a5187b40a48a86e08942b98aeb31fd86aa4c9
arnaub <abesorasans@gmail.com>
2021-09-01 16:09:37 +0200

    create seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,33 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     PetsApi.Repo.insert!(%PetsApi.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+defmodule PetsApi.DatabaseSeeder do
+  alias PetsApi.Owners
+  alias PetsApi.Owners.Owner
+  alias PetsApi.Pets
+
+  alias PetsApi.Repo
+  @owners_count 200
+  @pets_count 20
+
+  def run do
+    delete_data()
+    create_owners()
+  end
+
+  defp create_owners() do
+    Enum.map(1..@owners_count, fn _ ->
+      {:ok, owner} = Owners.create_owner(%{name: Faker.Person.name()})
+      create_pets(owner)
+    end)
+  end
+
+  defp create_pets(owner) do
+    Enum.map(1..@pets_count, fn _ ->
+      Pets.create_pet(%{name: Faker.Cat.name(), owner_id: owner.id})
+    end)
+  end
+
+  defp delete_data do
+    Repo.delete_all(Owner)
+  end
+end
+
+PetsApi.DatabaseSeeder.run()
